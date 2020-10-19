df39da59bc45df3ca7959f18bf90f9106daec0ab
hossain1527 <hossain.sci2015@gmail.com>
2020-10-19 01:41:44 +0100

    Change Color on Click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import randomcolor from 'randomcolor'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor () {
+    super()
+    this.state = {
+      color: "",
+      textcolor: "",
+      counter: 0
+    }
+    
+    this.changeColor = this.changeColor.bind(this)
+ 
+  }
+  
+  changeColor () {
+    this.setState(prevState => {
+      return {
+        color: randomcolor(),
+        textcolor: randomcolor(),
+        counter: prevState.counter + 1
+        
+      }
+    })
+  }
+
+  render () {
+    return (
+        
+      <div className="main-container" >
+              
+              <div style={{backgroundColor: this.state.color}}>
+                <button style={{color:this.state.color}} onClick={this.changeColor}>Change</button>
+                <h2 style={{color:this.state.textcolor}}>Click <hr />No {this.state.counter}</h2>
+                
+             </div>
+             
+            </div>
+       
+    ) 
+  }
 }
 
-export default App;
+
+export default App
