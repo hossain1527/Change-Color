a70ce9d2dd706978fd8995f01b3373ee101f0d4f
hossain1527 <hossain.sci2015@gmail.com>
2020-10-19 01:52:25 +0100

    Updates

---
+++ precache-manifest.2e66a02a15ea0fb1d2258fef00b8b6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939863ee6db14ada2316a2b7a74fd5a6",
+    "url": "/Change-Color/index.html"
+  },
+  {
+    "revision": "b3963987fe7ead3d53f4",
+    "url": "/Change-Color/static/css/main.19ff8f19.chunk.css"
+  },
+  {
+    "revision": "108c0328c3f1ac9b27ad",
+    "url": "/Change-Color/static/js/2.e15fd202.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Change-Color/static/js/2.e15fd202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3963987fe7ead3d53f4",
+    "url": "/Change-Color/static/js/main.dde998ca.chunk.js"
+  },
+  {
+    "revision": "2c6fa7ce4c03b5e20524",
+    "url": "/Change-Color/static/js/runtime-main.aa06e2d5.js"
+  }
+]);
